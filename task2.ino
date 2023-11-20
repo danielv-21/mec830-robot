a2affdc278769135698e42765141afe0d3b5bddf
daniel <ventura.daniel29@gmail.com>
2023-11-20 17:48:24 -0500

    Create task2.ino
    Added code to perform task 2
    Used delay to estimate distance travelled and angle rotation

---
+++ task2.ino
@@ -0,0 +1,66 @@
+/*****Define pins used by motor driver*****/
+#define PWMA 5
+#define PWMB 6
+#define STBY 3
+#define AIN1 8
+#define BIN1 7
+
+/*****Motion objects*****/
+void forward(){
+  digitalWrite(STBY,HIGH);
+  digitalWrite(AIN1,HIGH);
+  digitalWrite(BIN1,HIGH);
+}
+
+void backward(){
+  digitalWrite(STBY,HIGH);
+  digitalWrite(AIN1,LOW);
+  digitalWrite(BIN1,LOW); 
+}
+
+void stop(){
+  digitalWrite(STBY,LOW);
+}
+
+void turnRight(){
+  digitalWrite(STBY,HIGH);
+  digitalWrite(AIN1,HIGH);
+  digitalWrite(BIN1,LOW);
+}
+
+void turnLeft(){
+  digitalWrite(STBY,HIGH);
+  digitalWrite(AIN1,LOW);
+  digitalWrite(BIN1,HIGH);
+}
+
+/*****MAIN PROGRAM*****/
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(PWMA,OUTPUT);
+  pinMode(PWMB,OUTPUT);
+  pinMode(AIN1,OUTPUT);
+  pinMode(BIN1,OUTPUT);
+  pinMode(STBY,OUTPUT);
+  analogWrite(PWMA, 90);
+  analogWrite(PWMB, 90);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  forward();          //move forward for 138 cm
+  delay(3600);         //stop at p1
+  turnLeft();        //turn right
+  delay(600);         //stop at 90deg
+  stop();
+  delay(250);
+  
+  forward();           //move forward for 138 cm
+  delay(3600);         //stop at p2
+  turnRight();         //turn left
+  delay(600);         //stop at 90 deg
+  forward();          //move forward for 138 cm
+  delay(3600);         //stop at p3
+  stop();  
+  delay(250);
+}
