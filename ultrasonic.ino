0c88b1337774f49fdd88bae87c4ebf0dc08c054a
daniel <ventura.daniel29@gmail.com>
2023-11-20 17:47:07 -0500

    Create ultrasonic.ino
    Added code to use ultrasonic sensor

---
+++ ultrasonic.ino
@@ -0,0 +1,21 @@
+#include <NewPing.h>
+
+/*****Define ultrasonic sensor pins*****/
+#define trigger 13
+#define echo 12
+#define max_distance 200
+
+NewPing sonar(trigger,echo,max_distance);
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  delay(5);
+  int cm = sonar.ping_cm();          //ping_cm() returns integer
+  Serial.print("Ping: ");
+  Serial.println(cm);
+}
