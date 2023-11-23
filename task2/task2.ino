7bcb58b89ed7b7cae072bbd9039cba22097c90d6
daniel <ventura.daniel29@gmail.com>
2023-11-22 21:31:55 -0500

    Update task2.ino
    Added switch cases for main loop

--- task2/task2.ino
+++ task2/task2.ino
@@ -1,3 +1,27 @@
+#include <NewPing.h>
+#include "MPU9250.h"
+#include <Servo.h>
+
+/*****Servo*****/
+Servo myServo;
+
+int servopin = 11;
+int initpos = 90;
+
+/*****MPU9250*****/
+MPU9250 mpu;
+
+float refAngle;
+float yawAngle;
+float relAngle;
+
+/*****Define ultrasonic sensor pins*****/
+#define trigger 13
+#define echo 12
+#define max_distance 200
+
+NewPing sonar(trigger,echo,max_distance);
+
 /*****Define pins used by motor driver*****/
 #define PWMA 5
 #define PWMB 6
@@ -10,12 +34,16 @@ void forward(){
   digitalWrite(STBY,HIGH);
   digitalWrite(AIN1,HIGH);
   digitalWrite(BIN1,HIGH);
+  analogWrite(PWMA, 75);
+  analogWrite(PWMB, 75);
 }
 
 void backward(){
   digitalWrite(STBY,HIGH);
   digitalWrite(AIN1,LOW);
   digitalWrite(BIN1,LOW); 
+  analogWrite(PWMA, 50);
+  analogWrite(PWMB, 50);
 }
 
 void stop(){
@@ -26,41 +54,99 @@ void turnRight(){
   digitalWrite(STBY,HIGH);
   digitalWrite(AIN1,HIGH);
   digitalWrite(BIN1,LOW);
+  analogWrite(PWMA, 50);
+  analogWrite(PWMB, 50);
 }
 
 void turnLeft(){
   digitalWrite(STBY,HIGH);
   digitalWrite(AIN1,LOW);
   digitalWrite(BIN1,HIGH);
+  analogWrite(PWMA, 50);
+  analogWrite(PWMB, 50);
 }
+
+int count = 1;
+float cm;
 
 /*****MAIN PROGRAM*****/
 void setup() {
   // put your setup code here, to run once:
+  Serial.begin(115200);
+
+  /*****Initialize motor*****/
   pinMode(PWMA,OUTPUT);
   pinMode(PWMB,OUTPUT);
   pinMode(AIN1,OUTPUT);
   pinMode(BIN1,OUTPUT);
   pinMode(STBY,OUTPUT);
-  analogWrite(PWMA, 90);
-  analogWrite(PWMB, 90);
+
+  /*****Initialize servo*****/
+  myServo.attach(servopin);
+  myServo.write(initpos);
+
+  /*****Initialize MPU9250*****/
+  Wire.begin();
+  delay(2000);
+
+  if (!mpu.setup(0x68)) {  // change to your own address
+    while (1) {
+      Serial.println("MPU connection failed. Please check your connection with `connection_check` example.");
+      delay(5000);
+    }
+  }
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  forward();          //move forward for 138 cm
-  delay(3600);         //stop at p1
-  turnLeft();        //turn right
-  delay(600);         //stop at 90deg
-  stop();
-  delay(250);
+  measureRelAngle();
   
-  forward();           //move forward for 138 cm
-  delay(3600);         //stop at p2
-  turnRight();         //turn left
-  delay(600);         //stop at 90 deg
-  forward();          //move forward for 138 cm
-  delay(3600);         //stop at p3
-  stop();  
-  delay(250);
+  switch (count){
+    case 1:
+      if (refAngle == yawAngle){
+        count = 2;
+        break;
+      }
+    case 2:
+      delay(5);
+      cm = sonar.ping_cm();
+//      Serial.print("Distance = ");
+//      Serial.println(cm);
+      
+      if(cm >= 15){
+        forward();
+      }
+      else if(cm <15){
+        stop();
+        count = 3;
+        break;
+      }
+    case 3:
+      Serial.print("RelAngle = ");
+      Serial.print(relAngle);
+      Serial.print("  -  Count = ");
+      Serial.println(count);
+  }
+  
+  
+}
+
+float measureRelAngle(){
+  if (mpu.update()) {
+    static uint32_t prev_ms = millis();
+    if (millis() > prev_ms + 25) {
+      yawAngle = mpu.getYaw();
+      prev_ms = millis();
+
+      if (prev_ms > 15000 && prev_ms < 15050){          // wait for 15s to save initial angle
+        refAngle = yawAngle;
+//        Serial.print("Reference Angle: ");
+//        Serial.print(refAngle, 2);
+      }
+
+      relAngle = refAngle - yawAngle;
+//      Serial.print("Relative Angle ");
+//      Serial.println(relAngle,2);
+    }
+  }
 }
